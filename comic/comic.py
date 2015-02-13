afdbfcc916a942b8617a794c1934720ad05fa86e
Qiu Xiang <xiang.qiu@qq.com>
2015-02-13 17:26:20 +0800

    实现dmzj的基本数据获取

--- comic/comic.py
+++ comic/comic.py
@@ -2,9 +2,9 @@
 
 
 def load_site(classname):
-    return getattr(__import__('sites.' + classname), classname)
+    return getattr(__import__('sites.' + classname), classname).Site
 
 
 if __name__ == '__main__':
     comic = load_site('dmzj')
-    print comic.search('妖精')
+    print comic.search('妖精')[1].get_chapters()[0].get_images()
--- comic/sites/dmzj.py
+++ comic/sites/dmzj.py
@@ -1,20 +1,44 @@
-import requests
 import json
+import re
+import requests
+import execjs
 from bs4 import BeautifulSoup
 
 
-def search(keyword):
-    results = []
-    for item in json.loads(requests.get('http://s.acg.178.com/comicsum/search.php?s=' + keyword).content[20:-1]):
-        results.append(comic({
-            'description': item['description'],
-            'url': item['comic_url'],
-            'author': item['authors'],
-            'cover': item['cover'],
-            'name': item['name'],
-        }))
-    return results
+class Site:
+    def search(self, keyword):
+        results = []
+        for item in json.loads(requests.get('http://s.acg.178.com/comicsum/search.php?s=' + keyword).content[20:-1]):
+            if item['hidden'] == '0':
+                results.append(Comic({
+                    'description': item['description'],
+                    'url': item['comic_url'],
+                    'author': item['authors'],
+                    'cover': item['cover'],
+                    'name': item['name'],
+                }))
+        return results
 
 
-def comic(metadata):
-    return metadata
+class Comic:
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+    def get_chapters(self):
+        results = []
+        soup = BeautifulSoup(requests.get(self.metadata['url']).content)
+        for item in soup.select('.cartoon_online_border a'):
+            results.append(Chapter({
+                'url': 'http://manhua.dmzj.com' + item.get('href'),
+                'name': item.text,
+            }))
+        return results
+
+
+class Chapter:
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+    def get_images(self):
+        return ['http://images.dmzj.com/' + item for item in json.loads(execjs.exec_(re.search(
+            r'(eval.*\))', requests.get(self.metadata['url']).content).group(1) + ';return pages'))]
